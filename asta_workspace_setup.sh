8c7f6da1fc3c36a2aed985910fa2c9559cd07097
DavidWuthier <dwuthier@gmail.com>
2020-10-08 19:41:14 +0200

    Find ROS distro

--- asta_workspace_setup.sh
+++ asta_workspace_setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-for DISTRO in noetic melodic kinetic; do
-  if [ -e /opt/ros/$DISTRO ]; then
-    CURRENT_DISTRO=$DISTRO
-    break
-  fi
-done
-
-echo "distro detected: $CURRENT_DISTRO"
+sudo apt install git python3-catkin-tools python3-vcstool
 
 patch () {
   wget https://raw.githubusercontent.com/DavidWuthier/asta_workspace_setup/main/$1.patch
@@ -15,22 +8,34 @@ patch () {
   rm $1.patch
 }
 
-cd ~
-if [ ! -e asta_ws ]; then
-  mkdir -p asta_ws/src
-  cd asta_ws
-  catkin init
-  catkin config --extend /opt/ros/$CURRENT_DISTRO
-  wget https://raw.githubusercontent.com/DavidWuthier/asta_workspace_setup/main/asta_ws.repos
-  vcs import < asta_ws.repos
-  cd src/vrpn
-  git submodule init
-  git submodule update
-  patch catkinize_package
-  cd ../vrpn_client_ros
-  patch fix_cmake_module_path
-  #catkin build
+for DISTRO in noetic melodic kinetic; do
+  if [ -e /opt/ros/$DISTRO ]; then
+    CURRENT_DISTRO=$DISTRO
+    break
+  fi
+done
+
+if [ -n "$CURRENT_DISTRO" ]; then
+  echo "setting up workspace for ROS $CURRENT_DISTRO"
+  cd ~
+  if [ ! -e asta_ws ]; then
+    mkdir -p asta_ws/src
+    cd asta_ws
+    catkin init
+    catkin config --extend /opt/ros/$CURRENT_DISTRO
+    wget https://raw.githubusercontent.com/DavidWuthier/asta_workspace_setup/main/asta_ws.repos
+    vcs import < asta_ws.repos
+    cd src/vrpn
+    git submodule init
+    git submodule update
+    patch catkinize_package
+    cd ../vrpn_client_ros
+    patch fix_cmake_module_path
+    catkin build
+  else
+    echo "ASTA workspace already exists"
+  fi
 else
-  echo "ASTA workspace already exists"
+  echo "No ROS installation found"
 fi
 
